d91af2783652eedf0bea8625d342efd6fe14ad15
Hugo Quiñónez <hugo_2555@hotmail.com>
2018-07-06 11:19:42 -0600

    Iniciando correciones puntual

--- Serlaux/src/assets/js/puntual.js
+++ Serlaux/src/assets/js/puntual.js
@@ -1,97 +1,92 @@
 function CalcularPuntual() {
-    
+    alert("No pos si");
+
     var hours = $("#horas").val(); //pendiente
     hours = parseInt(hours);
-    
-    var kms = $('#distancia').val();
-    kms = kms.replace('km', '');
-    kms = kms.replace(',','.');    
+
+    var kms = $("#distancia").val();
+    kms = kms.replace("km", "");
+    kms = kms.replace(",", ".");
     kms = parseFloat(kms);
-    
+
     var provinceCost = 4;
-    switch (true){
-        case (kms < 20): 
+    switch (true) {
+        case kms < 20:
             provinceCost = 4;
             break;
-        case (kms < 30): 
+        case kms < 30:
             provinceCost = 5;
             break;
-        case (kms < 40): 
+        case kms < 40:
             provinceCost = 6;
             break;
-        case (kms < 50): 
+        case kms < 50:
             provinceCost = 7;
-            break;        
+            break;
         default:
             provinceCost = 0;
     }
-    
-    var leaderPrice = 4;
-    var vacuumPrice = 4;
-    var equipmentPrice = 4;
-    var othersPrice = 4;
+
+    $("#provinceCost").text(provinceCost);
+
+    var leaderPrice = 25;
+    var vacuumPrice = 25;
+    var equipmentPrice = 20;
 
     var needsLeader = $("#leader").is(":checked");
-    var needsVacuum =  $("#vacuum").is(":checked");
+    var needsVacuum = $("#vacuum").is(":checked");
     var needsEquipment = $("#basicEquipment").is(":checked");
-    var needsOthers = $("#others").is(":checked");
-    
+
+    var total = 0;
+
     if (provinceCost > 0) {
-        var finalHourRate = ((13 * hours) + provinceCost) / hours;
-        $(".totalHrs").text(hours);
-
-        
-        
-        var subTotal = (finalHourRate * hours);
-        var total = 0;
-
-        if (needsEquipment || needsVacuum || needsLeader || needsOthers) { $("#rowAditional").slideDown() }
-
-        if (needsLeader) { // escalera
-            $("#rowLeader").slideDown("slow");
-            $("#totalLeader").text(leaderPrice.toFixed(2) + " €");
-            total = total + leaderPrice;
-        } else {
-            $("#rowLeader").slideUp("slow");
-        }
-
-        if (needsEquipment) { // escalera
-            $("#rowBasicEquipment").slideDown("slow");
-            $("#totalEquipment").text(equipmentPrice.toFixed(2) + " €");
-            total = total + equipmentPrice;
-        } else {
-            $("#rowBasicEquipment").slideUp("slow");
-        }
-
-        if (needsVacuum) { // escalera
-            $("#rowVacuum").slideDown("slow");
-            $("#totalVacuum").text(vacuumPrice.toFixed(2) + " €")
-            total = total + vacuumPrice;
-        } else {
-            $("#rowVacuum").slideUp("slow");
-        }
-        
-        if (needsOthers) { // escalera
-            $("#rowOthers").slideDown("slow");
-            var othersText = $("#othersText").val();
-            $("#othersDescription").text(othersText.trim());
-            $("#totalOthers").text(othersPrice.toFixed(2) + " €");
-            total = total + othersPrice;
-        } else {
-            $("#rowOthers").slideUp("slow");
-        }
-
-        $("#subTotal").text(subTotal .toFixed(2) + " €");
+        hours = $("#hours").val();
+        var subTotal = hours * 4;
         total = total + subTotal;
+
+        //Descuento si tiene las 2
+        if (needsLeader && needsVacuum) {
+            leaderPrice = 20;
+            vacuumPrice = 20;
+        } else {
+            leaderPrice = 25;
+            vacuumPrice = 25;
+        }
+
+        if (needsLeader) {
+            total = total + leaderPrice;
+        }
+
+        if (needsEquipment) {
+            total = total + equipmentPrice;
+        }
+
+        if (needsVacuum) {
+            total = total + vacuumPrice;
+        }
+
         $("#total").text(total.toFixed(2) + " €");
-        $("#quoteTable").fadeIn(1000);
     } else {
-        alert('Su ubicación, está fuera de rango para nuestros servicios. \n Puede hablar con alguno de nuestros asesores.')
+        alert(
+            "Su ubicación, está fuera de rango para nuestros servicios. \n Puede hablar con alguno de nuestros asesores."
+        );
     }
 }
 
-$("#others").click(function(){
-    if ($("#others").is(":checked")) {
-        $("#othersText").focus();    
-    }        
+var othersControl = $("#others");
+othersControl.click(function() {
+    if (othersControl.is(":checked")) {
+        $("#othersText").slideDown();
+        $("#othersText").focus();
+    } else {
+        $("#othersText").slideUp();
+    }
 });
+
+function Intentar(){
+    $("#miprueba").text("hola");
+}
+
+$("#miprueba").onchange(
+    () => { alert('aqui estamos en el 91')}
+)
